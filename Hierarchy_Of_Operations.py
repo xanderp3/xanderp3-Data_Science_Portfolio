2d5f7f32107a25f1e53bcd0f66dbcffc0bd08bb7
xanderp3 <149524499+xanderp3@users.noreply.github.com>
2026-01-13 10:29:55 -0500

    Add Hierarchy of Operations example in Python
    This script demonstrates the hierarchy of operations in Python, including exponentiation and modulus operations.

---
+++ Hierarchy_Of_Operations.py
@@ -0,0 +1,31 @@
+# Raising a numeric data type to a power that is another numeric datatype - Example:
+
+print(2 ** 3)
+
+# Even though the following expression looks a bit weird, because it has a mathematical value, python wil calculate it.
+
+print(2 ** 3.5)
+
+# What is the remainder when 8 is divided by 3?
+# The mod (or modulus) operator can be used to answer this question.
+# For example, the remainder when 8 is divided by 3 can be expressed as 8 % 3.
+# THe % symbo is read as "mod".
+print(8 % 3)
+
+print(8.0 % 3)
+
+# Hierarchy of Operations:
+# 1. Grouping (Parenthesis)
+# 2. Exponents (**)
+# 3. Multiplication (*) / Division (/ or //)
+# 4. Addition (+) / Subtraction (-)
+
+# Left Side Binding / Right Side Binding
+
+# 3 - 2 + 1 < Left Side Binding
+
+# 2 ** 3 ** 2 < Right Side Binding
+
+
+# SImplify:
+2 * 3 ** 2 // 6
