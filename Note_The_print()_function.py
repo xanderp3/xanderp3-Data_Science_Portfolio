a278fd868cf02c78ccc07a4813e2f81a74ceacd8
xanderp3 <149524499+xanderp3@users.noreply.github.com>
2026-01-13 10:29:02 -0500

    Create Note_The_print()_function.py with examples
    Added a script demonstrating the usage of the print() function in Python with explanations of its parameters.

---
+++ Note_The_print()_function.py
@@ -0,0 +1,37 @@
+
+# notes_the_print()_function.py
+
+# The anatomy of the print() function.
+
+# The print() function is used to display "things" or data to the console.
+
+# print() function parameters:
+
+# 1. The first set of parameters: "things" or data you want to display:
+# 2. The next parameter is sep. sep tells us what to display in between each of the first parameters.
+# * The default value of sep is ' '.
+# The sep parameter receives its value using a keyword argument passing method.
+# This set of parameters receive their values using a positional arugment passing method.
+
+# 3. The end parameter tells us what to display at the end of the print.
+# The default value for end is '\n'
+# * The end paramter receives its value using a keyword argument passing method.
+
+
+print('Nestor', end= '*')
+print('Zaviel', end= '*')
+print('Javien')
+
+
+# *---*---*---*
+
+print("*", "*", "*", "*", sep = '___')
+
+print('*', end= '___')
+print('*', end= '___')
+print('*', end= '___')
+print('*', end= '___')
+
+
+
+print('Zaviel')
